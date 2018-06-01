7e9fb18a099d643cee828e9f813bc3f6a35d3863
kyleman <kyleborah1234321@gmail.com>
2018-06-01 13:54:10 -0500

    beginning VoiceOver speech support.

--- src/macos/engines.c
+++ src/macos/engines.c
@@ -6,9 +6,13 @@
 export int sayIsAvailable (void) ;
 export int saySay (const char *, int) ;
 export int sayStop (void) ;
+export int voIsAvailable (void) ;
+export int voSay (const char *, int) ;
+export int voStop (void) ;
 
 const engine engines = {
-    { .name = "Apple Say", .isAvailable = sayIsAvailable, .unload=NULL .say=saySay, .stop=sayStop, .braille=NULL, .setValue=NULL, .getValue=NULL, .setString=NULL, .getString=NULL }
+    { .name = "Apple Say", .isAvailable = sayIsAvailable, .unload=NULL .say=saySay, .stop=sayStop, .braille=NULL, .setValue=NULL, .getValue=NULL, .setString=NULL, .getString=NULL },
+        { .name = "Apple VoiceOver", .isAvailable = voIsAvailable, .unload=NULL .say=voSay, .stop=voStop, .braille=NULL, .setValue=NULL, .getValue=NULL, .setString=NULL, .getString=NULL }
 }
 
 const int numEngines = sizeof(engines)/sizeof(engine) -1;
---
+++ src/macos/vo.c
@@ -0,0 +1,21 @@
+//add header here
+
+#include <stdlib.h>
+#include "../../include/UniversalSpeech.h"
+
+export bool voIsavailable (void) {
+    return true:
+}
+
+export bool voSay (const char * msg, int inturrupt) {
+    system();
+    return true;
+
+}
+
+export bool voStop (void) {
+    return true;
+}
+
+
+}
